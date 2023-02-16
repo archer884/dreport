8034e6c5df3d8a98fff0d86cad5b390988bc9a18
John M Archer <archer884@zoho.com>
2023-02-16 13:04:19 -0600

    Maybe?

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
-use std::{process, io, fmt};
+use std::{fmt, fs, io, path::PathBuf, process};
 
 use clap::Parser;
-use fmtsize::{FmtSize, Conventional};
+use fmtsize::{Conventional, FmtSize};
 
 #[derive(Debug, Parser)]
 struct Args {
     root: String,
 }
 
-struct PathInfo {
-    name: String,
-    size: u64,
+struct DirInfo {
+    path: PathBuf,
+    len: u64,
 }
 
-impl fmt::Display for PathInfo {
+impl DirInfo {
+    fn new(path: impl Into<PathBuf>) -> Self {
+        Self {
+            path: path.into(),
+            len: 0,
+        }
+    }
+}
+
+impl fmt::Display for DirInfo {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let name = &self.name;
-        let size = self.size.fmt_size(Conventional);
+        let name = self.path.display();
+        let size = self.len.fmt_size(Conventional);
         write!(f, "{name}\t{size}")
     }
 }
@@ -38,6 +47,32 @@ fn run(args: &Args) -> io::Result<()> {
     Ok(())
 }
 
-fn walk(root: &str) -> io::Result<Vec<PathInfo>> {
-    todo!()
+fn walk(root: &str) -> io::Result<Vec<DirInfo>> {
+    let mut paths = get_directories(root)?;
+    paths.iter_mut().for_each(populate_dir_size);
+    Ok(paths)
+}
+
+fn populate_dir_size(info: &mut DirInfo) {
+    let len: u64 = walkdir::WalkDir::new(&info.path)
+        .into_iter()
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+            let meta = entry.metadata().ok()?;
+            meta.file_type().is_file().then(|| meta.len())
+        })
+        .sum();
+    info.len = len;
+}
+
+fn get_directories(root: &str) -> io::Result<Vec<DirInfo>> {
+    fs::read_dir(root)?
+        .map(|entry| entry.and_then(|entry| entry.file_type().map(|t| (t, entry.path()))))
+        .filter_map(|entry| {
+            entry
+                .map(|(t, path)| t.is_dir().then_some(path))
+                .transpose()
+        })
+        .map(|path| path.map(DirInfo::new))
+        .collect()
 }
